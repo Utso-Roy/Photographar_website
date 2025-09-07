3d1ad6ab356326553c2e30ffbac05e11e0e32705
Utso Roy <royutso003@gmail.com>
2025-09-07 22:19:31 +0600

    add professional responsive Login page with modern styling

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'photographar_website',
+  webDir: 'out',
+  
+};
+
+export default config;
---
+++ src/app/pages/register/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Register() {
+  return (
+    <div className='px-5 py-18'>Register</div>
+  )
+}
